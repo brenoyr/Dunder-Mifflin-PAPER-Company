522aaa538012e2a69259646523ded094a5893482
brenoyr <breno_yr@hotmail.com>
2020-10-17 03:31:12 -0500

    File dir and checksum based on mode selection

--- 02 Partition Analysis/main.py
+++ 02 Partition Analysis/main.py
@@ -6,18 +6,20 @@ if len(sys.argv) < 2:
     print "Missing mode: -m for MBR, -g for GPT"
     exit()
 
-# assigning correct checksum based on mode (MBR or GPT)
+# assigning correct checksum and file directory based on mode (MBR or GPT)
 mode = sys.argv[1]
 if mode == "-m":
+    fileDir = 'mbr_dump.iso'
     checksum = "a8a0e1dd8799459e6288b918d16b6efe2ef68809c7084f2dc968ec967d4574f3"
 elif mode == "-g":
+    fileDir = 'gpt_dump.iso'
     checksum = "5bf5860dfda9dd8cd13eb6d001c6667c43be34424bbf60bc62a722479c0bfb14"
 else:
     print "Mode not supported, try \"-m\" for MBR, or \"-g\" for GPT"
     exit()
 
 # opening file and reading it to isoFile
-with open('mbr_dump.iso', 'r') as f:
+with open(fileDir, 'r') as f:
     isoFile = f.read()
 
 # checking integrity of .iso file
@@ -29,4 +31,4 @@ if isoHash != checksum:
 # entering .iso hex contents into a list
 hex_list = ["{:02x}".format(ord(c)) for c in isoFile]
 
-print hex_list
+# print len(hex_list)
